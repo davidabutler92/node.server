229d52a72f1aecc9396829c9b761b3c485c7228b
davidabutler92 <davidabutler92@yahoo.com>
2020-11-02 13:55:11 -0800

    add routes to pet and user with json

---
+++ server.js
@@ -0,0 +1,39 @@
+const express = require('express'); // how we import in node
+const app = express();
+const port = process.env.PORT || 3000;
+const cors = require('cors');
+ 
+app.use(cors());
+
+app.get('/pet', (req, res) => {
+  console.log(new Date());
+  res.json([
+    {
+      pet: 'Great Dane',
+    },
+    {
+      name: 'Snowy',
+    },
+    {
+      weight: '100lbs',
+    },
+  ]);
+});
+
+app.get('/user', (req, res) => {
+  res.json([
+    {
+      fisrtName: 'David',
+    },
+    {
+      lastName: 'Butler',
+    },
+    {
+      age: 28,
+    },
+  ]);
+});
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
